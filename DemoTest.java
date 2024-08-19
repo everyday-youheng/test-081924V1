707bb3e4d2612fbe5cc662282cebe0eea79e74fa
everyday-youheng <weishengmiaomeng@sina.com>
2024-08-19 22:57:42 +0800

    修改DemoTest.java文件
    添加了一行代码

--- DemoTest.java
+++ DemoTest.java
@@ -1,5 +1,6 @@
 public class TheardTest01 {
     public static void main(String[] args) {
         System.out.print("Hello GitHub ..... ");
+        System.out.print("Hello GitHub 666 ..... ");
     }
 }
